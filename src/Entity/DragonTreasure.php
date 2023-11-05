4cbcc3a3fd8d999de707e8a8aa5a63a879f8d51d
Vladyslav Slyzhuk <vlad.slyzhuk@gmail.com>
2023-11-05 21:33:45 +0200

    changed name property to readonly

--- src/Entity/DragonTreasure.php
+++ src/Entity/DragonTreasure.php
@@ -13,6 +13,7 @@ use Carbon\Carbon;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\SerializedName;
 
 #[ORM\Entity(repositoryClass: DragonTreasureRepository::class)]
 #[ApiResource(
@@ -66,7 +67,8 @@ class DragonTreasure
     #[Groups(['treasure:read'])]
     private bool $isPublished = false;
 
-    public function __construct() {
+    public function __construct(string $name = null) {
+        $this->name = $name;
         $this->plunderedAt = new \DateTimeImmutable();
     }
 
@@ -78,13 +80,6 @@ class DragonTreasure
     public function getName(): ?string
     {
         return $this->name;
-    }
-
-    public function setName(string $name): static
-    {
-        $this->name = $name;
-
-        return $this;
     }
 
     public function getDescription(): ?string
@@ -100,6 +95,7 @@ class DragonTreasure
     }
 
     #[Groups(['treasure:write'])]
+    #[SerializedName('description')]
     public function setTextDescription(string $description): static
     {
         $this->description = nl2br($description);
